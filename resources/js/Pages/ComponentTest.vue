e668fcc67092f0f419099604e78f4c2c9a7f4d50
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2025-05-07 21:56:00 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,21 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Lable from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+</script>
+
+<template>
+<GuestLayout>
+<Label value="title-name">
+title
+</Label>
+<Input 
+  modelValue="初期値"
+  @update:modelValue="emitTest"
+  >
+
+</Input>
+</GuestLayout>
+</template>

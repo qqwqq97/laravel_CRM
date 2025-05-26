b7e3a31d4ba5cf896ec95d3078cd4a60ce7a7008
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2025-05-26 21:41:39 +0900

    customers

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CustomerController;
 use Inertia\Inertia;
 
 Route::resource('items', ItemController::class)->middleware(['auth', 'verified']);
+
+Route::resource('customers', CustomerController::class)->middleware(['auth', 'verified']);
+
 
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');

799bb072e4c0577221bcbaadb90e0ec5f63acfaf
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2025-06-21 15:44:50 +0900

    vue-chart-3

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AnalysisController;
 use Inertia\Inertia;
 
 Route::resource('items', ItemController::class)->middleware(['auth', 'verified']);
@@ -15,7 +16,7 @@ Route::resource('customers', CustomerController::class)->middleware(['auth', 've
 
 Route::resource('purchases', PurchaseController::class)->middleware(['auth', 'verified']);
 
-
+Route::get('analysis', [AnalysisController::class, 'index'])->name('analysis');
 
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');

0a0971f052b90ff63f457dc0b2c868bf7da1e6d6
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2025-07-03 21:52:01 +0900

    rfm

---
+++ app/Services/RFMService.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+
+class RFMService
+{
+  public static function rfm($subQuery, $rfmPrms)
+  {     
+        $subQuery = $subQuery ->groupBy('id')
+        ->selectRaw('id, customer_id,
+        customer_name, SUM(subtotal) as
+        totalPerPurchase, created_at');
+
+
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id, customer_name,
+        max(created_at) as recentDate,
+        DATEDIFF(now(), max(created_at)) as recency,
+        count(customer_id) as frequency,
+        sum(totalPerPurchase) as monetary');      
+
+        // dd($subQuery);
+
+        // $rfmPrms = [
+        // 14, 28, 60, 90, 7, 5, 3, 2, 300000, 200000, 100000,
+        // 30000 ];
+
+
+        $subQuery = DB::table($subQuery)
+        ->selectRaw('customer_id, customer_name,
+        recentDate, recency, frequency, monetary,
+        case
+        when recency < ? then 5
+        when recency < ? then 4
+        when recency < ? then 3
+        when recency < ? then 2
+        else 1 end as r,
+        case
+        when ? <= frequency then 5
+        when ? <= frequency then 4
+        when ? <= frequency then 3
+        when ? <= frequency then 2
+        else 1 end as f,
+        case
+        when ? <= monetary then 5
+        when ? <= monetary then 4
+        when ? <= monetary then 3
+        when ? <= monetary then 2
+        else 1 end as m', $rfmPrms);
+
+        // dd($subQuery);
+        $totals = DB::table($subQuery)->count();
+        $rCount = DB::table($subQuery)
+                ->rightJoin('ranks', 'ranks.rank', '=', 'r')
+                ->groupBy('rank')
+                ->selectRaw('rank as r, count(r)')
+                ->orderBy('r', 'desc')
+                ->pluck('count(r)');
+
+        $fCount = DB::table($subQuery)
+                ->rightJoin('ranks', 'ranks.rank', '=', 'f')
+                ->groupBy('rank')
+                ->selectRaw('rank as f, count(f)')
+                ->orderBy('f', 'desc')
+                ->pluck('count(f)');
+
+        $mCount = DB::table($subQuery)
+                ->rightJoin('ranks', 'ranks.rank', '=', 'm')
+                ->groupBy('rank')
+                ->selectRaw('rank as m, count(m)')
+                ->orderBy('m', 'desc')
+                ->pluck('count(m)');
+        // dd($rCount, $fCount, $mCount, $totals);    
+
+        $eachCount = []; // Vue側に渡すようの空の配列
+        $rank = 5; // 初期値5
+
+        for($i = 0; $i < 5; $i++)
+        {
+        array_push($eachCount, [
+        'rank' => $rank, 'r' => $rCount[$i] ?? 0, 'f' => $fCount[$i] ?? 0, 'm' => $mCount[$i] ?? 0, ]);
+        $rank--; // rankを1ずつ減らす
+        }
+        // dd($totals, $eachCount, $rCount, $fCount, $mCount);
+
+        $data = DB::table($subQuery)
+        ->rightJoin('ranks', 'ranks.rank', '=', 'r')
+        ->groupBy('rank')
+        ->selectRaw('concat("r_", rank) as rRank,
+        count(case when f = 5 then 1 end ) as f_5,
+        count(case when f = 4 then 1 end ) as f_4,
+        count(case when f = 3 then 1 end ) as f_3,
+        count(case when f = 2 then 1 end ) as f_2,
+        count(case when f = 1 then 1 end ) as f_1')
+        ->orderBy('rRank', 'desc')
+        ->get();
+
+        return [$data, $totals, $eachCount];
+  }
+}
